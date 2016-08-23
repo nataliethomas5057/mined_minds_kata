7cdc76b700c895aef6f1abb0a9f4fddbc798bbd2
Natalie Thomas <Natalie Thomas>
2016-08-23 13:51:22 -0400

    1-100

--- print_to_screen.rb
+++ print_to_screen.rb
@@ -1,13 +1,12 @@
 number = 1
 100.times do 
-	if number %3 == 0 && number%5 == 0
+	if number % 15 == 0
 		puts "Mined Minds"
-
-	elsif number %3 == 0
-	puts 'Mined'
-	elsif number %5 == 0
+	elsif number % 3 == 0 
+		puts "Mined"
+	elsif number % 5 == 0
 		puts "Minds"
 	else puts number
-end
-number = number + 1
+	end
+	number = number + 1
 end
