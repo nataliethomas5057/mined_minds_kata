631af585902704bbd10de2c4501eadce22f92859
Natalie Thomas <Natalie Thomas>
2016-08-25 10:33:48 -0400

    1 returns 1

--- HW_test_mined_minds_function.rb
+++ HW_test_mined_minds_function.rb
@@ -4,5 +4,9 @@ require_relative "hw_mined_minds_function.rb"
 class HWTestMinedMindsFunction < Minitest::Test
 	def test_1_equals_1
 		assert_equal(1, 1)
-	end	
+	end
+
+	def test_1_returns_1
+		assert_equal(1, mined_minds(1)) 
+	end		
 end
