1951c5cc3f6fc45157a718c0b9a8487cd38462f5
Natalie Thomas <Natalie Thomas>
2016-08-25 10:36:35 -0400

    2 returns 2 with error

--- HW_test_mined_minds_function.rb
+++ HW_test_mined_minds_function.rb
@@ -9,4 +9,11 @@ class HWTestMinedMindsFunction < Minitest::Test
 	def test_1_returns_1
 		assert_equal(1, mined_minds(1)) 
 	end		
+
+	def test_2_returns_2
+		assert_equal(2, mined_minds(2))
+	end	
+
+
+
 end
