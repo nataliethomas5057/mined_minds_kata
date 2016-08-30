688c4d381112c2719bff6ad3e9b0a7f7a192bdc1
Natalie Thomas <Natalie Thomas>
2016-08-30 11:48:54 -0400

    decimal correct

--- test_8302016_class_function.rb
+++ test_8302016_class_function.rb
@@ -38,7 +38,7 @@ class TestDivisionFunction < Minitest::Test
 		assert_equal(3, divide(6, 2))
 	end
 	def test_decimals
-		assert_equal(1.8, divide(3.14, 2))
+		assert_equal(1.57, divide(3.14, 2))
 	end	
 
 
