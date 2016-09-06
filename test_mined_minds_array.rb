cae6c6bd481da6c5ead88bf9efe4ac72d247c84d
Natalie Thomas <Natalie Thomas>
2016-09-06 09:15:34 -0400

    array test for 1-100

--- test_mined_minds_array.rb
+++ test_mined_minds_array.rb
@@ -2,6 +2,12 @@ require "minitest/autorun"
 require_relative "mined_minds_array.rb"
 
 class TestArrayFunction < Minitest::Test
+	def test_array_with_100
+		result= mm_array
+		assert_equal(100, results.length)
+	end	
+
+
 	def test_first_element_is_1
 		result = mm_array()
 		assert_equal(1, mm_array[0])
