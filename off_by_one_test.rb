c428d2bb18d7042d8254cedeab0da31a3bde6e70
Natalie Thomas <Natalie Thomas>
2016-09-13 11:51:13 -0400

    off by one true test

--- off_by_one_test.rb
+++ off_by_one_test.rb
@@ -7,4 +7,9 @@ class TestWinningNumbers < Minitest::Test
 		winning_ticket = ["2234"]
 		assert_equal(false, off_by_one(my_ticket, winning_ticket)) 
 	end	
+	def test_tickets_match_returns_true
+		my_ticket = "1234"
+		winning_ticket = ["2234"]
+		assert_equal(true, off_by_one(my_ticket, winning_ticket))
+	end	
 end	
