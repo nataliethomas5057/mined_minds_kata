016a4077dc37a9e9939801b950078cf4f831518c
Natalie Thomas <Natalie Thomas>
2016-09-13 10:53:06 -0400

    mult matches w 2 failures

--- 9_13_winning_tickets_1.rb
+++ 9_13_winning_tickets_1.rb
@@ -1,7 +1,9 @@
 def find_winners(our_ticket, winning_tickets)
 	matches = []
-	if winning_tickets.include?(our_ticket)
+	winning_tickets.each do |winning_ticket| #make singular here, looks at each item in the array called winningtickets, and we calle ach winningticket
+	if winning_tickets == our_ticket
 		matches << our_ticket 
 	end
+end	
 	matches
 end	
